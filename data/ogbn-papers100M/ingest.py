52e3f2a107f53d548aa718d4f248ba908c37c172
Harry Wik <harry.wik@neo4j.com>
2026-02-09 12:14:09 +0100

    save on storage and download data

--- data/ogbn-papers100M/ingest.py
+++ data/ogbn-papers100M/ingest.py
@@ -3,19 +3,40 @@ from graphdatascience import GraphDataScience
 import numpy as np
 from tqdm import tqdm
 import os
+from ogb.nodeproppred import NodePropPredDataset
+from graphdatascience import GraphDataScience
+from pathlib import Path
+
+# Get the directory where the script is located
+SCRIPT_DIR = Path(__file__).resolve().parent
+
+print("Initializing OGB dataset...")
+dataset = NodePropPredDataset(name='ogbn-papers100M', root=SCRIPT_DIR)
+
+# PATH VALIDATION
+base_path = SCRIPT_DIR / "ogbn_papers100M" / "raw"
+feat_path = base_path / 'node-feat.npy'
+edge_path = base_path / 'edge.npy'
+
+for file in os.listdir(str(base_path)):
+    if file.endswith(".gz"):
+        file_to_rem = base_path / file
+        print(f"Removing compressed file to save space: {str(file)}")
+        os.remove(str(file_to_rem))
+
+NODE_CHUNK = 100_000
+EDGE_CHUNK = 1_000_000
 
 # Connect to GDS Enterprise
 gds = GraphDataScience("bolt://localhost:7687", auth=("neo4j", "database-password"))
 
-raw_dir = './ogbn_papers100M/raw'
-feats = np.load(os.path.join(raw_dir, 'node-feat.npy'), mmap_mode='r')
+feats = np.load(str(feat_path), mmap_mode='r')
 num_nodes = feats.shape[0]
 
-edge_path = os.path.join(raw_dir, "edge.npy")
-edges = np.load(edge_path, mmap_mode='r') 
+edges = np.load(str(edge_path), mmap_mode='r') 
 total_edges = edges.shape[1]
 
-def node_generator(chunk_size=100_000):
+def node_generator(chunk_size=NODE_CHUNK):
     for i in range(0, num_nodes, chunk_size):
         end = min(i + chunk_size, num_nodes)
         
@@ -26,7 +47,7 @@ def node_generator(chunk_size=100_000):
         
         yield pa.Table.from_arrays([node_ids, features], names=["nodeId", "features"])
 
-def edge_generator(chunk_size=1_000_000): # Adapt to RAM size
+def edge_generator(chunk_size=EDGE_CHUNK): # Adapt to RAM size
     for i in range(0, total_edges, chunk_size):
         end = min(i + chunk_size, total_edges)
         # OGB is usually (2, num_edges); slice and convert
@@ -38,8 +59,8 @@ def edge_generator(chunk_size=1_000_000): # Adapt to RAM size
 # Project to GDS Memory via Arrow Flight
 G, result = gds.alpha.graph.project.arrow(
     graph_name="papers_graph",
-    nodes=tqdm(node_generator(), total=num_nodes // 500_000, desc="Streaming Nodes"),
-    relationships=tqdm(edge_generator(), total=total_edges // 2_000_000, desc="Streaming Edges")
+    nodes=tqdm(node_generator(), total=num_nodes // NODE_CHUNK, desc="Streaming Nodes"),
+    relationships=tqdm(edge_generator(), total=total_edges // EDGE_CHUNK, desc="Streaming Edges")
 )
 
 # Export to Disk
