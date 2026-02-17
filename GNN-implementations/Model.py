d0c24bf48d294ced5f9d05a53a6ea1d5041198ee
Victor Pekkari <victor.pekkari@neo4j.com>
2026-02-17 11:48:22 +0100

    changed bad naming in Model.py

--- GNN-implementations/Model.py
+++ GNN-implementations/Model.py
@@ -3,11 +3,11 @@ from torch_geometric.nn import GCNConv
 from torch import Tensor
 
 class GCN(nn.Module):
-    def __init__(self, in_dim:int, hidden_dim:int, out_dim:int, nbr_classes:int):
+    def __init__(self, in_dim:int, hidden1_dim:int, hidden2_dim:int, out_dim:int, nbr_classes:int):
         super().__init__()
-        self.GCN1 = GCNConv(in_dim, hidden_dim)
-        self.GCN2 = GCNConv(hidden_dim, out_dim)
-        self.classifier = nn.Linear(out_dim, nbr_classes)
+        self.GCN1 = GCNConv(in_dim, hidden1_dim, aggr="mean")
+        self.GCN2 = GCNConv(hidden1_dim, hidden2_dim, aggr="mean")
+        self.classifier = nn.Linear(hidden2_dim, nbr_classes)
     def forward(self, X:Tensor, edge_index:Tensor) -> Tensor:
         X = self.GCN1(X, edge_index).relu()
         output_GCN1 = self.GCN2(X, edge_index).relu()
