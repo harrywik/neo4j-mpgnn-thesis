f6b883b05e442efb76f8c4054d9e0db63eb6b610
Victor Pekkari <victor.pekkari@neo4j.com>
2026-02-17 12:34:14 +0100

    optimized forward pass in model.py

--- GNN-implementations/Model.py
+++ GNN-implementations/Model.py
@@ -3,13 +3,13 @@ from torch_geometric.nn import GCNConv
 from torch import Tensor
 
 class GCN(nn.Module):
-    def __init__(self, in_dim:int, hidden1_dim:int, hidden2_dim:int, out_dim:int, nbr_classes:int):
+    def __init__(self, in_dim:int, hidden_dim1:int, hidden_dim2:int, nbr_classes:int):
         super().__init__()
-        self.GCN1 = GCNConv(in_dim, hidden1_dim, aggr="mean")
-        self.GCN2 = GCNConv(hidden1_dim, hidden2_dim, aggr="mean")
-        self.classifier = nn.Linear(hidden2_dim, nbr_classes)
+        self.GCN1 = GCNConv(in_dim, hidden_dim1)
+        self.GCN2 = GCNConv(hidden_dim1, hidden_dim2)
+        self.classifier = nn.Linear(hidden_dim2, nbr_classes)
     def forward(self, X:Tensor, edge_index:Tensor) -> Tensor:
-        X = self.GCN1(X, edge_index).relu()
-        output_GCN1 = self.GCN2(X, edge_index).relu()
-        logits = self.classifier(output_GCN1)
+        X = self.GCN1(X, edge_index).relu_()
+        X = self.GCN2(X, edge_index).relu_()
+        logits = self.classifier(X)
         return logits
