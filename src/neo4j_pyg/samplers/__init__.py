5a2c18d8eb43c88629241355b17a59bcea6c96e4
Victor Pekkari <victor.pekkari@neo4j.com>
2026-03-20 13:16:02 +0100

    nice plot

--- src/neo4j_pyg/samplers/__init__.py
+++ src/neo4j_pyg/samplers/__init__.py
@@ -1,5 +1,4 @@
 from .Neo4jEdgeModeSampler import Neo4jEdgeModeSampler
 from .Neo4jSampler import Neo4jSampler
-from .Neo4jSamplerV2 import Neo4jSamplerEquiv, Neo4jSamplerFast
 from .Neo4jNeighborSampler import Neo4jNeighborSampler
 from .OldNeighborSampler import OldNeighborSampler
