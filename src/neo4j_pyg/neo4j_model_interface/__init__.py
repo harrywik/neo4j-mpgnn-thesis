c223ba77bebcc38e45297844c0b8aa961462a143
Victor Pekkari <victor.pekkari@neo4j.com>
2026-04-10 09:22:39 +0200

    fixed bug in hybridaggregator, and refactored pre aggregation code

--- src/neo4j_pyg/neo4j_model_interface/__init__.py
+++ src/neo4j_pyg/neo4j_model_interface/__init__.py
@@ -2,9 +2,8 @@ from neo4j_pyg.neo4j_model_interface.create_inference_spec import (
     create_inference_spec,
     upload_inference_spec,
 )
-from neo4j_pyg.neo4j_model_interface.preagg_adapters import (
-    HybridAggModel,
-    PreAggModelWrapper,
-    to_preaggregated_first_layer,
-    to_hybrid_last_hop_gcn,
+from neo4j_pyg.neo4j_model_interface.adapters import (
+    PreAggAdapter,
+    PreAggGCNConvAdapter,
 )
+from neo4j_pyg.neo4j_model_interface.hybrid_model import HybridAggModel
